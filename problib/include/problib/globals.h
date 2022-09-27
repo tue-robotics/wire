81c8ad10f9a17b8c54d473cfcc38039e4ab99991
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2022-09-27 09:49:13 +0200

    (problib) Fix logical-not-parenthesis

--- problib/include/problib/globals.h
+++ problib/include/problib/globals.h
@@ -49,6 +49,6 @@
 #include "problib/datatypes.h"
 
 //#define assert_msg(_Expression, _Msg) if (!_Expression) { printf(_Msg); printf("\n"); throw std::logic_error(""); }
-#define assert_msg(_Expression, _Msg) do { if (!_Expression) { printf(_Msg); printf("\n"); assert(_Expression); } } while (false);
+#define assert_msg(_Expression, _Msg) do { if (!(_Expression)) { printf(_Msg); printf("\n"); assert(_Expression); } } while (false);
 
 #endif /* GLOBALS_H_ */
